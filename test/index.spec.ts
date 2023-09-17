25eb7a475efe86e512dd547c062a700aafc6ba70
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2023-09-17 16:36:00 -0400

    a

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,35 +1,36 @@
 import { spawn } from "child_process";
+import fs from "fs";
+
+const dir = "test/files";
+
+const files = ["objects.ts"];
 
 describe("Your Test Suite", () => {
-  it("should pass files through stdin to index.js", (done) => {
-    const childProcess = spawn("node", ["build/index.js"], {
-      stdio: "pipe", // Enable stdin, stdout, and stderr pipes
-    });
+  files.forEach((file) => {
+    const path = `${dir}/${file}`;
+    it(`${file} should pass files through stdin to index.js`, (done) => {
+      const data = fs.readFileSync(path, "utf8");
+      const childProcess = spawn("node", ["build/index.js"], {
+        stdio: "pipe", // Enable stdin, stdout, and stderr pipes
+      });
 
-    // Handle the output (stdout) from your script
-    childProcess.stdout.on("data", (data) => {
-      const output = data.toString();
-      expect(output).toContain("Expected Output");
-    });
+      childProcess.stdout.on("data", (data) => {
+        const output = data.toString();
+        console.log(output);
+        // expect(output).toContain("Expected Output");
+      });
 
-    // Handle errors (stderr)
-    childProcess.stderr.on("data", (data) => {
-      // Handle any errors from your script
-      console.error(data.toString());
-      done.fail(data.toString());
-    });
+      childProcess.on("exit", (code) => {
+        if (code === 0) {
+          done();
+        } else {
+          done.fail(`Process exited with code ${code}`);
+        }
+      });
 
-    // Handle process exit
-    childProcess.on("exit", (code) => {
-      if (code === 0) {
-        done();
-      } else {
-        done.fail(`Process exited with code ${code}`);
-      }
+      // Write files to the stdin of the child process
+      childProcess.stdin.write(data);
+      childProcess.stdin.end(); // Close stdin
     });
-
-    // Write files to the stdin of the child process
-    childProcess.stdin.write("file content here");
-    childProcess.stdin.end(); // Close stdin
   });
 });
